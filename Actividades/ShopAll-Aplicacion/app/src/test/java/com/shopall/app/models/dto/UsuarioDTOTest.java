fb0db0c6dcff7425bae645a7f4c9b56ba71fff45
ARGL <92689016+alejandro026@users.noreply.github.com>
2023-11-16 10:41:03 -0600

    Agregando teste

---
+++ Actividades/ShopAll-Aplicacion/app/src/test/java/com/shopall/app/models/dto/UsuarioDTOTest.java
@@ -0,0 +1,23 @@
+package com.shopall.app.models.dto;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+
+public class UsuarioDTOTest {
+
+    UsuarioDTO usuarioDTO;
+
+    @BeforeEach
+    void setUp(){
+        usuarioDTO= new UsuarioDTO();
+    }
+
+
+    @Test
+    private void testSetId(){
+
+    }
+
+}
